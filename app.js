950402ee5474e5261c9b8989a23cce3a5002b265
Erik Watson <evw@gloversure.co.uk>
2016-11-09 12:48:12 +0000

    Sketched out the basics - haven't tackled loops yet

---
+++ app.js
@@ -0,0 +1,63 @@
+let memory = [0]
+let pointer = 0
+
+function fuck_brains(input) {
+  let chars = input.split('')
+
+  chars.forEach((item) => {
+    switch(item) {
+      case '+': increment();  break;
+      case '-': decrement();  break;
+      case '<': left();       break;
+      case '>': right();      break;
+      case '[': open_loop();  break;
+      case ']': close_loop(); break;
+
+      default: // Everything else is ignored
+    }
+  })
+
+  return memory
+
+}
+
+function increment() {
+  console.log('Incrementing the current value by one')
+  memory[pointer] ++
+}
+
+function decrement() {
+  console.log('Decrementing the current value by one')
+  memory[pointer] --
+}
+
+function left() {
+  console.log('Moving the memory pointer left one address')
+  pointer --
+}
+
+function right() {
+  console.log('Moving the memory pointer right one address')
+  pointer ++
+
+  if(!memory[pointer]) {
+    addMoreMemory()
+  }
+}
+
+function open_loop() {
+  console.log('Opening a new loop')
+}
+
+function close_loop() {
+  console.log('Cloding an open loop')
+}
+
+function addMoreMemory() {
+  memory.push(0)
+}
+
+const input = '+++>++>+<<--'
+
+let output = fuck_brains(input)
+console.log(output)
