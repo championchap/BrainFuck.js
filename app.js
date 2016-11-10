8bd2dc95a5f820a8e4e1cec0dd6a786c16d37772
Erik Watson <championchap@gmail.com>
2016-11-10 19:34:26 +0000

    Beginning work on a parser for the language

--- app.js
+++ app.js
@@ -1,67 +1,68 @@
 let memory = [0]
 let pointer = 0
 
-function fuck_brains(input) {
-  let chars = input.split('')
+function fuckBrains (input) {
+  let memory = [0]
+  const chars = strip(input)
+  const tree = parse(chars)
 
-  chars.forEach((item) => {
-    switch(item) {
-      case '+': increment();  break;
-      case '-': decrement();  break;
-      case '<': left();       break;
-      case '>': right();      break;
-      case '[': open_loop();  break;
-      case ']': close_loop(); break;
-
-      default: // Everything else is ignored
-    }
-  })
+  console.log({ memory: memory, tree: tree })
 
   return memory
-
 }
 
-function increment() {
+function strip (input) {
+  const stripCharacters = /[^\[\]<>+-]/g
+
+  return input
+    .replace(stripCharacters, '')
+    .split('')
+}
+
+function parse (input) {
+  return {}
+}
+
+function increment () {
   console.log('Incrementing the current value by one')
   memory[pointer] ++
 }
 
-function decrement() {
+function decrement () {
   console.log('Decrementing the current value by one')
   memory[pointer] --
 }
 
-function left() {
+function left () {
   console.log('Moving the memory pointer left one address')
-  if(pointer > 0) {
-    pointer --
+  if (pointer > 0) {
+    pointer -= 1
   } else {
-    throw { error: 'Already at the first position in memory' }
+    throw new Error('Already at the first position in memory')
   }
 }
 
-function right() {
+function right () {
   console.log('Moving the memory pointer right one address')
-  pointer ++
+  pointer += 1
 
-  if(!memory[pointer]) {
+  if (!memory[pointer]) {
     addMoreMemory()
   }
 }
 
-function open_loop() {
+function openLoop () {
   console.log('Opening a new loop')
 }
 
-function close_loop() {
+function closeLoop () {
   console.log('Closing an open loop')
 }
 
-function addMoreMemory() {
+function addMoreMemory () {
   memory.push(0)
 }
 
-const input = '+++>++>+<<--<<<'
+const input = '[@$%@$%*@#$%][wer]pdfs-d9f0s9dfio23j5492805979234"sd"f\'sd\'fS"DF'
 
-let output = fuck_brains(input)
-console.log(output)
+fuckBrains(input)
